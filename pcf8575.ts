16bf13582631a208afb40c338c30ed7bd5d3ddb4
鹧鸪天 <sorc@qq.com>
2023-12-27 19:55:57 +0800

    支持翻转引脚

--- pcf8575.ts
+++ pcf8575.ts
@@ -3,8 +3,9 @@
  */
 //% weight=100 color=#0f00ff icon=""
 namespace PCF8575 {
-
     const CHIP_ADDRESS = 0x20
+    //翻转引脚 1-7 10-17 翻转后 7-1 17-10
+    let pinOverturn = 0
 
     export enum PinValue {
         //% block="高1"
@@ -21,7 +22,7 @@ namespace PCF8575 {
         P5 = 0x05,
         P6 = 0x06,
         P7 = 0x07,
-        
+
         P10 = 0x08,
         P11 = 0x09,
         P12 = 0x0a,
@@ -31,49 +32,66 @@ namespace PCF8575 {
         P16 = 0x0e,
         P17 = 0x0f
     }
-    
     /**
-        * 设置针脚数据
+        * 设置针脚电位
         * 参数 针脚 pin
-        * 参数 数值 value
+        * 参数 电位 value
         */
     //% pin.min=0 pin.max=256
     //% value.min=-9999 value.max=9999
-    //% blockId=setPinData block="设置针脚 %pin|的数值 %value"
-    export function setPinData(pin: PinIndex, value: PinValue): void {
-        const currentState = getPin();
-        if (pin > 7) {
-            pin = 15 - pin + 8;
-        } else {
-            pin = 7 - pin;
+    //% blockId=setPinValue block="设置针脚 %pin|的电位 %value"
+    export function setPinValue(pin: PinIndex, value: PinValue): void {
+        const currentState = getValue();
+        if (pinOverturn==1){
+            //翻转引脚
+            if (pin > 7) {
+                pin = 15 - pin + 8;
+            } else {
+                pin = 7 - pin;
+            }
         }
         const bit = 1 << 15 - pin;
         const newState = value == 1 ? (currentState | bit) : (currentState & (~bit & 0xffff));
-        const buffer2 = pins.createBuffer(2);
-        buffer2[0] = (newState >> 8) & 0xff;
-        buffer2[1] = newState & 0xff;
-        pins.i2cWriteBuffer(CHIP_ADDRESS, buffer2, false);
+        setValue(newState);
     }
     /**
-     * 读取针脚数据
+     * 读取针脚电位
      */
-    //% blockId=readPinValue block="读取针脚 %u 的数据"
-    export function readPinValue(pin: PinIndex): PinValue {
-        const currentState = getPin();
+    //% blockId=readPinValue block="读取针脚 %u 的电位"
+    export function getPinValue(pin: PinIndex): PinValue {
+        const currentState = getValue();
         const value = currentState & 1 << 15 - pin;
         return value == 0 ? PinValue.L : PinValue.H;
-    } 
-    function getPin() {
-        const buffer = pins.i2cReadBuffer(CHIP_ADDRESS, 2, false);
-        return buffer[0] >> 8 | buffer[1];
-    } 
+    }
+    /**
+    * 翻转引脚 1-7 10-17 翻转后 7-1 17-10
+    */
+    //% blockId=setPinOverturn block="翻转引脚"
+    export function setPinOverturn(): void {
+        pinOverturn=1;
+    }
+    /**
+    * 复位
+    */
+    //% blockId=reset block="复位"
+    export function reset(): void {
+        setValue(0);
+    }
     /**
      * 电位
      */
-    //% blockId=getPinValue block="电位 %u"
-    export function getPinValue(value: PinValue): PinValue {
+    //% blockId=pinValue block="电位 %u"
+    export function pinValue(value: PinValue): PinValue {
         return value;
     }
-
- 
+    function setValue(value: number) {
+        const buffer2 = pins.createBuffer(2);
+        buffer2[0] = (value >> 8) & 0xff;
+        buffer2[1] = value & 0xff;
+        pins.i2cWriteBuffer(CHIP_ADDRESS, buffer2, false);
+    }
+    function getValue() {
+        const buffer = pins.i2cReadBuffer(CHIP_ADDRESS, 2, false);
+        return buffer[0] << 8 | buffer[1];
+    }
 }
